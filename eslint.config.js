fec07bc375ebba4cb38843312be4c7679ae07efa
charmeee <mandugohigh@gmail.com>
2026-07-01 21:50:02 +0900

    chore: ESLint 9 flat config + Prettier 설정 정비
    - eslint-config-prettier 연동으로 포맷 충돌 규칙 비활성화
    - react/react-hooks/react-refresh 규칙 및 no-unused-vars(^_ 예외) 설정
    - prettier.config.js / .prettierignore 추가 (singleQuote, printWidth 100)

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,59 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'build', 'node_modules']),
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
   {
     files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier,
     ],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/no-unescaped-entities': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/explicit-function-return-types': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+  {
+    files: ['**/*.js'],
+    extends: [js.configs.recommended, prettier],
     languageOptions: {
-      globals: globals.browser,
+      globals: globals.node,
     },
   },
 ])
